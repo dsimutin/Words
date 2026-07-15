6528a70188e185ed51c08b8315561edeb5991a52
dsimutin <dimkasimutin@gmail.com>
2026-07-15 19:18:48 +0300

    Render cached home before refreshing statistics

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='words-shell-v5';
+const CACHE='words-shell-v6';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-words-v2-180.png','./icon-words-v2-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
